71907703f18b61ada29ce4aad2d3a9a128767727
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-03-18 17:52:47 +0100

    Pep8/pyflakes

--- src/imu.partnertable/imu/partnertable/partnertable.py
+++ src/imu.partnertable/imu/partnertable/partnertable.py
@@ -1,3 +1,4 @@
+import math
 from five import grok
 from plone.directives import dexterity, form
 from Acquisition import aq_inner
@@ -8,20 +9,18 @@ from imu.partnertable import MessageFactory as _
 
 from imu.partnertable.sglpartner import ISglPartner
 
-# Interface class; used to define content-type schema.
 
-class IPartnerTable(form.Schema):   
+class IPartnerTable(form.Schema):
     """
     Partner Folder
     """
     title = schema.TextLine(
-            title=_(u"Name"),
-            required=True,
+        title=_(u"Name"),
+        required=True,
     )
-    
     description = schema.Text(
-                  title=_(u"A short summary"),
-                  required=False,
+        title=_(u"A short summary"),
+        required=False,
     )
 
 
@@ -32,31 +31,32 @@ class PartnerTable(dexterity.Item):
 class PartnerTableView(grok.View):
     grok.context(IPartnerTable)
     grok.require('zope2.View')
-    grok.name('view')    
-    
+    grok.name('view')
+
     def haveSglPartner(self):
         return len(self.contained_sglpartner()) > 0
-    
+
     def partner(self):
-        import math
         results = IContentListing(self.contained_sglpartner())
         count = len(results)
-        rowcount = count/2.0
+        rowcount = count / 2.0
         rows = math.ceil(rowcount)
         matrix = []
         for i in range(int(rows)):
             row = []
             for j in range(2):
-                index = 2*i + j
+                index = 2 * i + j
                 if index <= int(count - 1):
                     cell = {}
                     cell['item'] = results[index]
                     row.append(cell)
             matrix.append(row)
         return matrix
-    
+
     def contained_sglpartner(self):
-        """ Return a list of contained teasers in order to construct a item scrollable. """
+        """ Return a list of contained teasers in order to construct a
+            item scrollable.
+        """
         context = aq_inner(self.context)
         catalog = getToolByName(context, 'portal_catalog')
         results = catalog(object_provides=ISglPartner.__identifier__,
